dfebecea7d137698399b77c54c11f5337e18b948
rsekhar617 <42176909+rsekhar617@users.noreply.github.com>
2018-11-30 20:31:43 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n ;
+clrscr();
+printf("numbers");
+scanf("%d" ,&n);
+if(n%2==0)
+{
+printf("Even");
+}
+else if
+n%2<0
+{
+printf("Invalid");
+}
+else if
+{
+printf("Odd");
+}
+getch();
+}
